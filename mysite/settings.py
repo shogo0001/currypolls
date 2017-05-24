8a6f16f6ba35f43b2ea5cd9db7f1898b6c30c822
shogo0001 <shogo0001@hotmail.com>
2017-05-24 10:49:20 +1000

    debug false

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2vimpk7%%t)vc@p^&eb5z&hjpai=pit6nuoatz_w8dw=y)ed9m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["currypolls.herokuapp.com"]
 
