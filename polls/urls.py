10e1be4dcbf9f6d5a9488a6a034b3c7e2cfa68e9
shogo0001 <shogo0001@hotmail.com>
2017-05-24 18:46:38 +1000

    next

--- polls/urls.py
+++ polls/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
   url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
   url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
   url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
-  url(r'^(?P<question_id>[0-9]+)/next/$', views.next, name='next'),
+#  url(r'^(?P<question_id>[0-9]+)/next/$', views.next, name='next'),
 
 #   # ex: /polls/
 #   url(r'^$', views.index, name='index'),
