ecbfbf8e3d01fdc1f34302f0e95c69600dd396ef
shogo0001 <shogo0001@hotmail.com>
2017-05-24 17:55:16 +1000

    bootstrap cdn

--- polls/urls.py
+++ polls/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
   url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
   url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 
-  # ex: /polls/5/
-  url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+#  # ex: /polls/5/
+#  url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
 
 #   # ex: /polls/
 #   url(r'^$', views.index, name='index'),
